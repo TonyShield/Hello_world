b000f3b2ab9f018d7d32788ef8cbb7752eed8a85
Tony Stark <tony@tony95.com>
2015-10-24 00:19:17 +0800

    Changes

--- GitHub_test/GitHub_test/main.cpp
+++ GitHub_test/GitHub_test/main.cpp
@@ -11,6 +11,7 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    std::cout << "gitHub test!\n";
+    std::cout << "gitHub test!!!\n";
+    
     return 0;
 }
