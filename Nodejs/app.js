779d3fdfd03253b43d9cca63634a8534b31b4098
Tony Stark <294586144@qq.com>
2015-11-20 03:42:59 +0800

    add

---
+++ Nodejs/app.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+http.createServer(
+	function(request, response)
+{
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("It's alive!");
+	response.end();
+}).listen(3000);
+
+$.post('/some_requested_resource', function(data) {
+  console.log(data);
+});
+
+// add 
